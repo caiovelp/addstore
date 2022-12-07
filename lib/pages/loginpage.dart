316de30554df3abc69a0d92ef00c37fe0b2dcf4e
Caio Paranhos <cparanhos@id.uff.br>
2022-12-07 12:34:13 -0300

    Tela de login adicionada

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,10 +10,81 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String? _username, _password;
+
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      textStyle: TextStyle(fontSize: 25),
+      elevation: 5,
+      minimumSize: Size(100, 30)
+    );
+
+    final ButtonStyle styleText = ElevatedButton.styleFrom(
+        textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+    );
+
     return Scaffold(
       appBar: AppBar(title: Text("Login"),),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset('images/logo.png'),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: TextFormField(
+                        onSaved: (newValue) => _username = newValue,
+                        decoration: InputDecoration(
+                          labelText: "Username",
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: TextFormField(
+                        onSaved: (newValue) => _password = newValue,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          border: OutlineInputBorder()
+                        ),
+                      ),
+                    ),
+                    ElevatedButton.icon(
+                        onPressed: () => {},
+                        style: style,
+                        icon: Icon(Icons.login_rounded),
+                        label: Text("Entrar")
+                    ),
+                  ],
+                )
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextButton(
+                      onPressed: () => {},
+                      style: styleText,
+                      child: Text("Registrar")
+                  ),
+                  TextButton(
+                      onPressed: () => {},
+                      style: styleText,
+                      child: Text("Ver anúncios"))
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
